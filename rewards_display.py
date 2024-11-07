3a374caea6cf1f048e5d109164c5bf32b4458acc
Richa Sinha <109168595+richasinha12@users.noreply.github.com>
2024-11-07 21:35:50 +0530

    Create rewards_display.py

---
+++ rewards_display.py
@@ -0,0 +1,8 @@
+# utils/rewards_display.py
+def fetch_rewards_data():
+    return {
+        "rewards": [
+            {"name": "Fuel Discount", "description": "10% off diesel fuel"},
+            {"name": "Campground Voucher", "description": "$20 off first night"}
+        ]
+    }
